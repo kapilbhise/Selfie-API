b55c0a92f05f2f6e342cfaa5037dcd5cc05ec4a1
Kapil Bhise <bhisekapil38@gmail.com>
2020-10-17 14:32:58 +0530

    environment variable

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const path = require('path');
 const http = require('http');
 const fs = require('fs');
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3000;
 const Datastore = require('nedb');
 const pathToData = path.resolve(__dirname, "db/db")
 const db = new Datastore({ filename: pathToData});
@@ -15,7 +15,7 @@ const app = express()
 // add logging middleware
 app.use(logger("dev"))
 
-// Handling JSON data 
+// Handling JSON data
 app.use(express.json({limit: '5mb',extended: true}));
 app.use(express.urlencoded({limit: '5mb', extended: true}));
 
@@ -36,23 +36,23 @@ app.get("/logs", (req, res) => {
 // Show all my submissions
 // our API
 // GET - /api
-app.get("/api", (req, res) => {    
+app.get("/api", (req, res) => {
     db.find({}, function (err, docs) {
         if(err){
             return err;
-        } 
+        }
         res.json(docs);
     });
 });
 
-/**  
- * 
+/**
+ *
  * when we post to the database we will
  * query the weatherjs and get our results
  * and append them to the incoming geocoordinates
- * sent from the client as well as the 
+ * sent from the client as well as the
  * selfie from the webcam.
- * */ 
+ * */
 app.post("/api", (req, res) => {
     // our unix timestamp
     const unixTimeCreated = new Date().getTime();
@@ -68,15 +68,15 @@ app.post("/api", (req, res) => {
     });
 })
 
-/**  
- * 
+/**
+ *
  * Export all images to PNG on the server
- * */ 
+ * */
 app.get("/export/all", (req, res) => {
     db.find({}).sort({'created':1}).exec(function (err, docs) {
         if(err){
             return err;
-        } 
+        }
         docs.forEach(item => {
             const outImage = item.image.replace(/^data:image\/png;base64,/, '');
             fs.writeFileSync(path.resolve(__dirname, `./exports/all/${item.created}.png`) , outImage, 'base64');
@@ -92,4 +92,3 @@ app.get("/export/all", (req, res) => {
 http.createServer(app).listen(port, () =>{
     console.log(`see the magic at: http://localhost:${port}`)
 })
-
